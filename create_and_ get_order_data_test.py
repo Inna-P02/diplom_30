cb04a1495a34c1449b5dc8d9c20d96ea2929b6fe
Inna-P02 <Inna0202@ya.ru>
2025-06-10 13:15:18 +0400

    Update create_and_ get_order_data_test.py

--- create_and_ get_order_data_test.py
+++ create_and_ get_order_data_test.py
@@ -7,22 +7,19 @@ import sender_stand_request
 
 def get_new_order(order_body):
     response = sender_stand_request.post_new_order(order_body)
-    assert response.status_code == 201
-    return response.json() 
+    assert response.status_code == 201 
+    return response.json()
 
 
-def get_order_track(track):
-    order_response = requests.get(configuration.URL_SERVICE + configuration.CREATE_ORDER + str(track))
+def order_track(track):
+    order_response = sender_stand_request.get_order_track(track)
     return order_response
-    
-   
+
 
 def test_create_and_get_order():
-    order_body = data.order_body
-    response_create = sender_stand_request.post_new_order(order_body) 
-    assert response_create.status_code == 201
-    track_number = response_create.json().get("track")  
+    order_body = data.order_body  
+    order_response = get_new_order(order_body)
+    track_number = order_response.get("track")
     print(f"Трек-номер: {track_number}")
-    order_data = get_order_track(track_number) 
+    order_data = order_track(track_number)
     assert order_data.status_code == 200
-    
