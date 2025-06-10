c8ca3a8464212805d15f820a6923a07379760b41
Inna-P02 <Inna0202@ya.ru>
2025-06-10 13:15:45 +0400

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -5,12 +5,14 @@ import data
 
 
 def post_new_order(order_body):
-    order_body = data.order_body
     response = requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER,  
                          json=order_body,
                          )  
     return response
 
+def get_order_track(track):
+    order_response = requests.get(configuration.URL_SERVICE + configuration.ORDER_TRACK + str(track))
+    return order_response
 
 
 
